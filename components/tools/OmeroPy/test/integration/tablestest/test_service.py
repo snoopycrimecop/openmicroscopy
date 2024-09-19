3d5add672b1d6e49df7d73c4364f421df476d4fc
Sébastien Besson <sbesson@glencoesoftware.com>
2024-09-19 12:28:08 +0100

    Add test covering all semantics of start,end passed to table.read()

--- components/tools/OmeroPy/test/integration/tablestest/test_service.py
+++ components/tools/OmeroPy/test/integration/tablestest/test_service.py
@@ -928,6 +928,12 @@ class TestTables(ITest):
         with pytest.raises(omero.ApiUsageException):
             twoColumnFiveRowTable.read([3], 0, 0)
 
+        # start, end cannot be null
+        with pytest.raises(ValueError):
+            twoColumnFiveRowTable.read([0], None, 0)
+        with pytest.raises(ValueError):
+            twoColumnFiveRowTable.read([0], 0, None)
+
     def testSliceInvalidInput(self, twoColumnFiveRowTable):
 
         # colNumbers must match table columns
@@ -937,3 +943,71 @@ class TestTables(ITest):
         # rowNumbers must match table rows
         with pytest.raises(omero.ApiUsageException):
             twoColumnFiveRowTable.slice([], [6])
+
+    def testReadStartEnd(self, twoColumnFiveRowTable):
+
+        # [1, 2, 3, 4, 5][-1:5] = [5]
+        data = twoColumnFiveRowTable.read([0], -1, 5)
+        assert 1 == len(data.columns)
+        assert [5] == data.columns[0].values
+        assert [4] == data.rowNumbers
+
+        # Special casing around [0:0]
+        data = twoColumnFiveRowTable.read([0], 0, 0)
+        assert 1 == len(data.columns)
+        assert [1, 2, 3, 4, 5] == data.columns[0].values
+        assert [0, 1, 2, 3, 4] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:2] = [1, 2]
+        data = twoColumnFiveRowTable.read([0], 0, 2)
+        assert 1 == len(data.columns)
+        assert [1, 2] == data.columns[0].values
+        assert [0, 1] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:5] = [1, 2, 3, 4, 5]
+        data = twoColumnFiveRowTable.read([0], 0, 5)
+        assert 1 == len(data.columns)
+        assert [1, 2, 3, 4, 5] == data.columns[0].values
+        assert [0, 1, 2, 3, 4] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:10] = [1, 2, 3, 4, 5]
+        data = twoColumnFiveRowTable.read([0], 0, 10)
+        assert 1 == len(data.columns)
+        assert [1, 2, 3, 4, 5] == data.columns[0].values
+        assert [0, 1, 2, 3, 4] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:-1] = [1, 2, 3, 4]
+        data = twoColumnFiveRowTable.read([0], 0, -1)
+        assert 1 == len(data.columns)
+        assert [1, 2, 3, 4] == data.columns[0].values
+        assert [0, 1, 2, 3] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:-4] = [1]
+        data = twoColumnFiveRowTable.read([0], 0, -4)
+        assert 1 == len(data.columns)
+        assert [1] == data.columns[0].values
+        assert [0] == data.rowNumbers
+
+        # [1, 2, 3, 4, 5][0:-5] = []
+        data = twoColumnFiveRowTable.read([0], 0, -5)
+        assert 1 == len(data.columns)
+        assert [] == data.columns[0].values
+        assert 0 == len(data.rowNumbers)
+
+        # [1, 2, 3, 4, 5][2:1] = []
+        data = twoColumnFiveRowTable.read([0], 2, 1)
+        assert 1 == len(data.columns)
+        assert [] == data.columns[0].values
+        assert 0 == len(data.rowNumbers)
+
+        # [1, 2, 3, 4, 5][2:2] = []
+        data = twoColumnFiveRowTable.read([0], 2, 2)
+        assert 1 == len(data.columns)
+        assert [] == data.columns[0].values
+        assert 0 == len(data.rowNumbers)
+
+        # [1, 2, 3, 4, 5][2:5] = [3, 4, 5]
+        data = twoColumnFiveRowTable.read([0], 2, 5)
+        assert 1 == len(data.columns)
+        assert [3, 4, 5] == data.columns[0].values
+        assert [2, 3, 4] == data.rowNumbers
